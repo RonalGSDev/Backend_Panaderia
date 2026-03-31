d9524ae45673be2181b208ad2258f266b691f5fd
RonalGSDev <gomezronal986@gmail.com>
2026-03-31 00:11:01 -0600

    Variables2

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     // Busca la variable, si no existe usa 'localhost' (para tu PC)
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'admin123',
-    database: process.env.DB_NAME || 'panaderia',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     
     // Solo activa SSL si la variable DB_SSL es 'true' (como en Aiven)
     ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : null,
